99cc1597bf2f052ebf3f9f36cc722e2532d7c3b3
Dominik Schweizer <dominikschweizer06@gmail.com>
2023-12-01 10:41:32 +0100

    grundbaustein

---
+++ src/Search.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+import data from "./data";
+
+const Search = () => {
+  const [state, setState] = useState(data);
+
+  return (
+    <section className="py-4 container">
+      <div className="row justify-content-center">
+        <div className="col-12 mb-5">
+          <div className="mb-3 col-4 mx-auto text-center">
+            <label className="form-lable h4">Search</label>
+            <input type="text" className="from-control" />
+          </div>
+        </div>
+
+        {data.cardData.map((item, index) => {
+          return (
+            <div className="col-11 col-md-6 col-lg-3 mx-0 mb-4">
+              <div className="card p-0 overflow-hidden h-100 shadow">
+                <img src={item.img} className="card-img-top" />
+                <div className="card-body">
+                  <h5 className="card-title">{item.title}</h5>
+                  <p className="card-text">{item.desc}</p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Search;
---
+++ src/data.js
@@ -0,0 +1,40 @@
+import img1 from "./img/img1.jpg";
+
+
+
+const data = {
+    cardData:[
+        {
+            id: 1,
+            img: img1,
+            title: "blog 1",
+            desc: "desc 1"
+        },
+        {
+            id: 2,
+            img: img1,
+            title: "blog 2",
+            desc: "desc 2"
+        },
+        {
+            id: 3,
+            img: img1,
+            title: "blog 3",
+            desc: "desc 3"
+        },
+        {
+            id: 4,
+            img: img1,
+            title: "blog 4",
+            desc: "desc 4"
+        },
+        {
+            id: 5,
+            img: img1,
+            title: "blog 5",
+            desc: "desc 5"
+        },
+    ]
+}
+
+export default data;
