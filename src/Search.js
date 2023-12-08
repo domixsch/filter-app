f7fca0589b93293a5b59befc2582a742108d170d
Dominik Schweizer <dominikschweizer06@gmail.com>
2023-12-08 16:00:58 +0100

    finished product

--- src/Search.js
+++ src/Search.js
@@ -13,6 +13,10 @@ const Search = () => {
     setProductNameFilter(event.target.value);
   };
 
+
+  {
+    //Data Search Funktion
+  }
   let dataSearch = data.cardData.filter((item) => {
     return (
       item.title.toLowerCase().includes(filter.toLowerCase()) &&
@@ -27,6 +31,10 @@ const Search = () => {
       <div className="row justify-content-center">
         <div className="col-12 mb-5">
           <div className="mb-3 col-4 mx-auto text-center">
+
+            {
+            //Searchbar
+            }
             <label className="form-lable h4">Search</label>
             <input
               type="text"
@@ -34,6 +42,10 @@ const Search = () => {
               value={filter}
               onChange={searchText}
             />
+
+            {
+            //DropDown menue
+            }
             <select value={productNameFilter} onChange={handleProductNameFilter}>
               <option value="">All Products</option>
               {uniqueProductNames.map((productName) => (
@@ -45,6 +57,10 @@ const Search = () => {
           </div>
         </div>
 
+
+            {
+            //Cardbody mit Img und Title
+            }
         {dataSearch.map((item, index) => {
           return (
             <div className="col-11 col-md-6 col-lg-3 mx-0 mb-4" key={index}>
